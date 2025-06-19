4b4ec840be2f635ba7f483dc20ea10fc4c12ddbd
manish-adtani1 <manishadtani99@gmail.com>
2025-06-19 13:14:49 +0530

    fix: update CORS origin to production endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ import taskRoute from './routes/task.route.js'
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://secondtask-opal.vercel.app'
+    'https://secondtask-1dxz.onrender.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
