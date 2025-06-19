f12162ee9c5574fa67765bd69fcfcd5a68a5e295
manish-adtani1 <manishadtani99@gmail.com>
2025-06-19 13:01:54 +0530

    fix: update CORS configuration to allow production origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,11 +17,15 @@ import taskRoute from './routes/task.route.js'
 
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE',"OPTIONS"],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With', 'Origin',"X-Access-Token"],
+  origin: [
+    'http://localhost:5173',
+    'https://secondtask-opal.vercel.app'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With', 'Origin', 'X-Access-Token'],
 }));
+
 
 
 
